f6db5a95caf69ccffa4293f9f8ecbf31d6278a0e
Irea Mosquera-Lois <i.mosquera-lois22@imperial.ac.uk>
2024-09-11 09:29:49 +0100

    restructure mlff code

--- compchem_toolkit/mlff/vasp.py
+++ compchem_toolkit/mlff/vasp.py
@@ -14,6 +14,7 @@ import matplotlib.ticker as mtick
 
 from pymlff.io import ml_ab_from_trajectory
 
+
 def get_structure_from_config(config: pymlff.core.Configuration):
     # Get list of species in configuration
     species = []
