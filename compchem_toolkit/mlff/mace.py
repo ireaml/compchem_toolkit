04fce9043a5089bffce48f3d5a02e293a9b467b7
Irea Mosquera-Lois <i.mosquera-lois22@imperial.ac.uk>
2024-09-12 15:46:09 +0100

    automate sampling

--- compchem_toolkit/mlff/mace.py
+++ compchem_toolkit/mlff/mace.py
@@ -1,3 +1,6 @@
+from ase.io import read
+
+
 def parse_training_errors(filename):
     with open (filename, "r") as f:
         lines = f.readlines()
@@ -7,3 +10,18 @@ def parse_training_errors(filename):
     rmse_F = [float(line.split("RMSE_F=")[1].split("meV / A")[0]) for line in epoch_lines]
     rmse_S = [float(line.split("RMSE_stress_per_atom=")[1].split("meV / A^3")[0]) for line in epoch_lines]
     return epoch_number, rmse_E, rmse_F, rmse_S
+
+
+def get_mace_db_from_mlab(
+    mlab, key_energy="REF_energy", key_forces="REF_forces", key_stress="REF_stress"
+):
+    mlab.write_extxyz(filename="MLAB.extxyz", stress_unit="eV/A^3")
+    traj = read("MLAB.extxyz", index=":")
+    os.remove("MLAB.extxyz")
+    # Set info for each atom
+    for i in range(len(traj)):
+        traj[i].arrays[key_forces] = traj[i].get_forces() # Needed for mace parsing!
+        traj[i].info[key_energy] = traj[i].calc.results["free_energy"] # Parsed from VASP ML_AB with pymlff #.calc.results["free_energy"]
+        traj[i].info[key_stress] = traj[i].get_stress() # voigt_6_to_full_3x3_stress(traj[i].get_stress())
+        traj[i].info["original_energy"] = traj[i].calc.results["energy"]
+    return traj
--- compchem_toolkit/mlff/vasp.py
+++ compchem_toolkit/mlff/vasp.py
@@ -107,8 +107,45 @@ def get_mlab_from_traj(traj, n_clusters, threshold_init=0.15, plot=True, index_f
         return (index_struc, index_site)
 
     def perform_direct_sampling(
-        descriptors_by_element, n_clusters, threshold_init=0.15, plot=True
+        descriptors_by_element, n_clusters, threshold_init=0.15, plot=True, score=True
     ):
+        def calculate_all_FCS(all_features, selected_indexes, b_bins=100):
+            def calculate_feature_coverage_score(all_features, selected_indexes, n_bins=100):
+                selected_features = all_features[selected_indexes]
+                n_all = np.count_nonzero(
+                    np.histogram(all_features, bins=np.linspace(min(all_features), max(all_features), n_bins))[0]
+                )
+                n_select = np.count_nonzero(
+                    np.histogram(selected_features, bins=np.linspace(min(all_features), max(all_features), n_bins))[0]
+                )
+                return n_select / n_all
+            select_scores = [
+                calculate_feature_coverage_score(all_features[:, i], selected_indexes, n_bins=b_bins)
+                for i in range(all_features.shape[1])
+            ]
+            return select_scores
+
+        def plot_scores(scores_MPF_DIRECT):
+            # fig, ax = plt.subplots(figsize=(15, 4))
+            x = np.arange(len(scores_MPF_DIRECT))
+            x_ticks = [f"PC {n+1}" for n in range(len(x))]
+
+            plt.figure(figsize=(15, 4))
+            plt.bar(
+                x + 0.6,
+                scores_MPF_DIRECT,
+                width=0.3,
+                label=f"Coverage score = {np.mean(scores_MPF_DIRECT):.3f}",
+            )
+            # plt.xticks(x + 0.45, x_ticks, size=16)
+            plt.yticks(np.linspace(0, 1.0, 6), size=16)
+            plt.ylabel("Coverage score", size=20)
+            plt.xlabel("Principal component", size=20)
+            # Remove xticks
+            plt.gca().xaxis.set_major_locator(plt.NullLocator())
+            plt.legend(shadow=True, loc="lower right", fontsize=16)
+            return plt
+
         # Get the symbols of the atoms in the structure
         DIRECT_sampler = DIRECTSampler(
             structure_encoder=False,
@@ -124,10 +161,15 @@ def get_mlab_from_traj(traj, n_clusters, threshold_init=0.15, plot=True, index_f
             print(
                 f"DIRECT selected {len(DIRECT_selection[s]['selected_indexes'])} from {len(DIRECT_selection[s]['PCAfeatures'])} environments "
             )
+            #print("Shape DIRECT_selection[s]['PCAfeatures']:", DIRECT_selection[s]['PCAfeatures'].shape)
             all_features = DIRECT_selection[s]["PCAfeatures"]
             selected_indexes = DIRECT_selection[s]["selected_indexes"]
             if plot:
                 plot_PCAfeature_coverage(all_features, selected_indexes)
+            if score:
+                n_pcas = all_features.shape[1]
+                scores_DIRECT = calculate_all_FCS(all_features, selected_indexes, b_bins=n_pcas)
+                ax = plot_scores(scores_DIRECT)
         return DIRECT_selection
 
     symbols = [list(OrderedDict.fromkeys(a.get_chemical_symbols())) for a in traj]
