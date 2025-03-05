a28005b6f0cfd3ed8db2b4c7e2ee553477073914
Irea Mosquera-Lois <i.mosquera-lois22@imperial.ac.uk>
2025-03-05 08:28:32 +0000

    update

--- compchem_toolkit/mlff/mace.py
+++ compchem_toolkit/mlff/mace.py
@@ -13,17 +13,17 @@ def parse_training_errors(filename, multihead=False):
         epoch_number = [int(line.split("Epoch ")[1].split(":")[0]) for line in epoch_lines_1]
         rmse_E_1 = [float(line.split("RMSE_E_per_atom=")[1].split("meV")[0]) for line in epoch_lines_1]
         rmse_F_1 = [float(line.split("RMSE_F=")[1].split("meV / A")[0]) for line in epoch_lines_1]
-        rmse_S_1 = [float(line.split("RMSE_stress_per_atom=")[1].split("meV / A^3")[0]) for line in epoch_lines_1]
+        rmse_S_1 = [float(line.split("RMSE_stress=")[1].split("meV / A^3")[0]) for line in epoch_lines_1]
         rmse_E_2 = [float(line.split("RMSE_E_per_atom=")[1].split("meV")[0]) for line in epoch_lines_2]
         rmse_F_2 = [float(line.split("RMSE_F=")[1].split("meV / A")[0]) for line in epoch_lines_2]
-        rmse_S_2 = [float(line.split("RMSE_stress_per_atom=")[1].split("meV / A^3")[0]) for line in epoch_lines_2]
+        rmse_S_2 = [float(line.split("RMSE_stress=")[1].split("meV / A^3")[0]) for line in epoch_lines_2]
         return epoch_number, rmse_E_1, rmse_F_1, rmse_S_1, rmse_E_2, rmse_F_2, rmse_S_2
     else:
         epoch_lines = [line for line in lines if "Epoch" in line][1:]
         epoch_number = [int(line.split("Epoch ")[1].split(":")[0]) for line in epoch_lines]
         rmse_E = [float(line.split("RMSE_E_per_atom=")[1].split("meV")[0]) for line in epoch_lines]
         rmse_F = [float(line.split("RMSE_F=")[1].split("meV / A")[0]) for line in epoch_lines]
-        rmse_S = [float(line.split("RMSE_stress_per_atom=")[1].split("meV / A^3")[0]) for line in epoch_lines]
+        rmse_S = [float(line.split("RMSE_stress=")[1].split("meV / A^3")[0]) for line in epoch_lines]
         return epoch_number, rmse_E, rmse_F, rmse_S
 
 
